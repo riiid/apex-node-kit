327b66b527b0038413c9a474ced2b3d9c1946ef2
chitacan <chitacan@gmail.com>
2016-07-02 03:46:23 +0900

    support api gateway deployment with terraform.

---
+++ infrastructure/dev/main.tf
@@ -0,0 +1,42 @@
+variable "aws_region" {}
+
+variable "apex_environment" {}
+
+variable "apex_function_hello" {}
+
+/**
+ * resources
+ */
+
+resource "aws_api_gateway_rest_api" "api" {
+  name        = "apex-node-kit-api"
+  description = "api for apex-node-kit"
+}
+
+resource "aws_api_gateway_resource" "hello" {
+  rest_api_id = "${aws_api_gateway_rest_api.api.id}"
+  parent_id   = "${aws_api_gateway_rest_api.api.root_resource_id}"
+  path_part   = "hello"
+}
+
+module "hello-post" {
+  source          = "./api-gateway-method"
+  method          = "POST"
+  rest_api_id     = "${aws_api_gateway_rest_api.api.id}"
+  parent_id       = "${aws_api_gateway_rest_api.api.root_resource_id}"
+  resource_id     = "${aws_api_gateway_resource.hello.id}"
+  aws_region      = "${var.aws_region}"
+  lambda_function = "${var.apex_function_hello}"
+}
+
+module "deploy" {
+  source      = "./api-gateway-deploy"
+  rest_api_id = "${aws_api_gateway_rest_api.api.id}"
+  stage_name  = "${var.apex_environment}"
+  depends_id  = "${module.hello-post.id}"
+}
+
+/**
+ * outputs
+ */
+
