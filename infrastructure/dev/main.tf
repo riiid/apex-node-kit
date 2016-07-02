8648d7e68eb81d1eac341fc2dea3681e61b860c9
chitacan <chitacan@gmail.com>
2016-07-03 06:04:05 +0900

    print out deployed api gateway url.

--- infrastructure/dev/main.tf
+++ infrastructure/dev/main.tf
@@ -39,4 +39,6 @@ module "deploy" {
 /**
  * outputs
  */
-
+output "url" {
+  value = "https://${aws_api_gateway_rest_api.api.id}.execute-api.${var.aws_region}.amazonaws.com/${var.apex_environment}"
+}
