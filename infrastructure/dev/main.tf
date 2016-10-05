65f81e80693707655d68c00e22b3c9018a9e4519
chitacan <chitacan@gmail.com>
2016-10-06 01:21:55 +0900

    change common module location.

--- infrastructure/dev/main.tf
+++ infrastructure/dev/main.tf
@@ -18,8 +18,8 @@ resource "aws_api_gateway_resource" "graphql" {
   path_part   = "graphql"
 }
 
-module "hello-post" {
-  source          = "./api-gateway-method"
+module "graphql-post" {
+  source          = "../api-gateway-method"
   method          = "POST"
   rest_api_id     = "${aws_api_gateway_rest_api.api.id}"
   parent_id       = "${aws_api_gateway_rest_api.api.root_resource_id}"
@@ -29,7 +29,7 @@ module "hello-post" {
 }
 
 module "deploy" {
-  source      = "./api-gateway-deploy"
+  source      = "../api-gateway-deploy"
   rest_api_id = "${aws_api_gateway_rest_api.api.id}"
   stage_name  = "${var.apex_environment}"
   depends_id  = "${module.graphql-post.id}"
