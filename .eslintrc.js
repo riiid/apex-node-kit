e143fe33d9b264a7503eb70a5e640fd191bd9c8e
chitacan <chitacan@gmail.com>
2017-02-14 18:23:49 +0900

    add eslint plugin for react.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
   "parser": "babel-eslint",
-  "extends": "google",
+  "plugins": ["react"],
+  "extends": ["google", "plugin:react/recommended"],
   "installedESLint": true,
   "env": {
     "node": true,
@@ -8,7 +9,9 @@ module.exports = {
   },
   "rules": {
     "camelcase": 0,
+    "require-jsdoc": 0,
     "comma-dangle": [2, "never"],
-    "arrow-parens": [2, "as-needed"]
+    "arrow-parens": [2, "as-needed"],
+    "react/jsx-uses-react": 1
   }
 };
