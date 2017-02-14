c02df503805b6fae45959e35dd2fbc56c4d30125
chitacan <chitacan@gmail.com>
2017-02-15 02:14:57 +0900

    update test cases.

--- functions/hello/__tests__/index.js
+++ functions/hello/__tests__/index.js
@@ -1,25 +1,82 @@
 import func from '../';
 
 describe('functions/hello', () => {
-  it('should response "text/html" with "GET" method', done => {
-    const event = {method: 'GET'};
-    const context = {};
-    func(event, context, (err, response) => {
-      expect(err).toBeNull();
-      expect(response['Content-Type']).toBe('text/html');
-      done();
+  describe('GET', () => {
+    it('should response "text/html" with "GET" request.', done => {
+      const event = {method: 'GET'};
+      const context = {};
+      func(event, context, (err, response) => {
+        expect(err).toBeNull();
+        expect(response['Content-Type']).toBe('text/html');
+        done();
+      });
     });
   });
 
-  it('should response "application/json" with "POST" method', done => {
-    const event = {
-      method: 'POST', body: {query: 'query {hello}'}
-    };
-    const context = {};
-    func(event, context, (err, response) => {
-      expect(err).toBeNull();
-      expect(response['Content-Type']).toBe('application/json');
-      done();
+  describe('POST', () => {
+    it('should response with "query {hello}"', done => {
+      const event = {
+        method: 'POST', body: {query: 'query {hello}'}
+      };
+      const context = {};
+      func(event, context, (err, response) => {
+        expect(err).toBeNull();
+        expect(response.result).toEqual({hello: 'hello'});
+        expect(response['Content-Type']).toBe('application/json');
+        done();
+      });
+    });
+
+    it('should response with "query {helloAsync}".', done => {
+      const event = {
+        method: 'POST', body: {query: 'query {helloAsync}'}
+      };
+      const context = {};
+      func(event, context, (err, response) => {
+        expect(err).toBeNull();
+        expect(response.result).toEqual({helloAsync: 'hello async'});
+        expect(response['Content-Type']).toBe('application/json');
+        done();
+      });
+    });
+
+    it('should response with "mutation".', done => {
+      const query = `
+        mutation {
+          createHello(prefix: "wow")
+        }
+      `;
+      const event = {method: 'POST', body: {query}};
+      const context = {};
+      func(event, context, (err, response) => {
+        expect(err).toBeNull();
+        expect(response.result).toEqual({createHello: 'wow world!'});
+        expect(response['Content-Type']).toBe('application/json');
+        done();
+      });
+    });
+
+    it('should response error with invalid query.', done => {
+      const event = {
+        method: 'POST', body: {query: 'query hello}'}
+      };
+      const context = {};
+      func(event, context, (err, response) => {
+        expect(err).not.toBeNull();
+        done();
+      });
+    });
+  });
+
+  describe('ETC', () => {
+    it('should response "text/plain" with unmatched request.', done => {
+      const event = {method: 'ANY'};
+      const context = {};
+      func(event, context, (err, response) => {
+        expect(err).toBeNull();
+        expect(response['Content-Type']).toBe('text/plain');
+        done();
+      });
     });
   });
 });
