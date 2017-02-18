6086396183fc1d982a6845657cb3c52acb470002
chitacan <chitacan@gmail.com>
2017-02-18 15:07:12 +0900

    update infrastructure.
     - rename resource.
     - change `aws_api_gateway_resource` path parth to `{proxy+}`.
     - change lambda integration type to `AWS_PROXY`.

--- infrastructure/dev/hello/main.tf
+++ infrastructure/dev/hello/main.tf
@@ -15,10 +15,10 @@ variable "authorization" {
 
 variable "role" {}
 
-resource "aws_api_gateway_resource" "hello" {
+resource "aws_api_gateway_resource" "res" {
   rest_api_id = "${var.api_id}"
   parent_id   = "${var.api_root_resource_id}"
-  path_part   = "hello"
+  path_part   = "{proxy+}"
 }
 
 /**
@@ -27,33 +27,24 @@ resource "aws_api_gateway_resource" "hello" {
 
 resource "aws_api_gateway_method" "method" {
   rest_api_id   = "${var.api_id}"
-  resource_id   = "${aws_api_gateway_resource.hello.id}"
+  resource_id   = "${aws_api_gateway_resource.res.id}"
   http_method   = "ANY"
   authorization = "${var.authorization}"
 }
 
 resource "aws_api_gateway_integration" "request_integration" {
   rest_api_id             = "${var.api_id}"
-  resource_id             = "${aws_api_gateway_resource.hello.id}"
+  resource_id             = "${aws_api_gateway_resource.res.id}"
   http_method             = "${aws_api_gateway_method.method.http_method}"
   integration_http_method = "POST"
-  type                    = "AWS"
+  type                    = "AWS_PROXY"
   uri                     = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/${var.lambda_function}/invocations"
   credentials             = "${var.role}"
-
-  request_templates = {
-    "application/json" = <<EOF
-{
-  "method": "$context.httpMethod",
-  "body": $input.json('$')
-}
-EOF
-  }
 }
 
 resource "aws_api_gateway_method_response" "200" {
   rest_api_id = "${var.api_id}"
-  resource_id = "${aws_api_gateway_resource.hello.id}"
+  resource_id = "${aws_api_gateway_resource.res.id}"
   http_method = "${aws_api_gateway_method.method.http_method}"
   status_code = "200"
 
@@ -67,23 +58,11 @@ resource "aws_api_gateway_method_response" "200" {
 
 resource "aws_api_gateway_integration_response" "response_integration" {
   rest_api_id = "${var.api_id}"
-  resource_id = "${aws_api_gateway_resource.hello.id}"
+  resource_id = "${aws_api_gateway_resource.res.id}"
   http_method = "${aws_api_gateway_method.method.http_method}"
   status_code = "${aws_api_gateway_method_response.200.status_code}"
-
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
-    "method.response.header.Access-Control-Allow-Methods" = "'GET,POST,PUT,DELETE,PATCH,OPTION'"
-    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
-    "method.response.header.Content-Type"                 = "integration.response.body.Content-Type"
-  }
-
-  response_templates = {
-    "text/html"        = "$input.path('$.result')"
-    "application/json" = "input.json('$.result')"
-  }
 }
 
 output "url" {
-  value = "https://${var.api_id}.execute-api.${var.region}.amazonaws.com/${var.environment}/${aws_api_gateway_resource.hello.path_part}"
+  value = "https://${var.api_id}.execute-api.${var.region}.amazonaws.com/${var.environment}/${aws_api_gateway_resource.res.path_part}"
 }
